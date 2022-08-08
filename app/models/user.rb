1ec2090e470c5784dd5f9f777a867bcd18afd842
shigeki310 <hinahisa01240301@gmail.com>
2022-08-08 17:45:39 +0900

    デバイスの導入

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, presence: true
+    validates :password, :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :email, presence: true
+  end
+end
