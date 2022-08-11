23cfe5e3e061d5e20d68c2b3578eb0906dbb2efb
shigeki310 <hinahisa01240301@gmail.com>
2022-08-11 18:00:03 +0900

    投稿まで

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,26 @@
 class PhotosController < ApplicationController
 
   def index
+    @photo = Photo.all.order(created_at: :desc)
   end
 
+  def new 
+    @photo = Photo.new
+  end
 
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :description, :image).merge(user_id: current_user.id)
+  end
+  
 end
--- db/migrate/20220809160213_create_photos.rb
+++ db/migrate/20220809160213_create_photos.rb
@@ -1,10 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-      t.string :nickname, null: false
       t.string :title, null: false
       t.text :description, null: false
-      t.text :image, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
